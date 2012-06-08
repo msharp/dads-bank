cbec78bb767165a10200cf87662e87b97ca21b75
max sharples <maxsharples@gmail.com>
2012-06-08 12:20:43 +1000

    a couple of tests for account transactions

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'credit account' do
+    a = Account.first
+
+    tc = a.transactions.count
+    ab = a.balance
+    a.credit(5)
+
+    a.reload
+
+    assert a.transactions.count == tc + 1
+    assert a.balance == ab + 5
+  end
+ 
+  test 'debit account' do
+    a = Account.first
+
+    tc = a.transactions.count
+    ab = a.balance
+    a.debit(5)
+
+    a.reload
+
+    assert a.transactions.count == tc + 1
+    assert a.balance == ab - 5
+  end
+
+
 end
